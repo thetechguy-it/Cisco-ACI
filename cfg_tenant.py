4b6037889667e27f7de25fddc9c8781233989210
rickynale <riccardo.nalesso10@gmail.com>
2022-06-17 00:34:38 +0200

    Cisco ACI

--- cfg_tenant.py
+++ cfg_tenant.py
@@ -5,7 +5,6 @@ session = Session(credentials.url, credentials.user, credentials.pwd)
 session.login()
 
 file = open("data.txt", "r")
-list_of_lists = []
 for line in file:
     stripped_line = line.strip()
     line_list = stripped_line.split()
@@ -16,17 +15,12 @@ for line in file:
     app = AppProfile(splitted_list[1], tenant)
     context = Context(splitted_list[2], tenant)
     bd = BridgeDomain(splitted_list[3], tenant)
-    epg = EPG(splitted_list[4], app)
+    bd_subnet = Subnet(splitted_list[5], bd)
+    bd_subnet.set_addr(splitted_list[4])
+    bd.add_subnet(bd_subnet)
+    bd.set_unknown_mac_unicast('flood') #Enable ARP Flooding AND L2 Unknown Unicast 
+    epg = EPG(splitted_list[5], app)
     bd.add_context(context)
     epg.add_bd(bd)
-    #bd.set_unicast_route('yes')
     tenant.push_to_apic(session)
 file.close()
-
-#bd.set_arp_flood('yes')
-#bd.set_unicast_route('no')
-#bd.set_unknown_mac_unicast
-#bd.set_unknown_multicast
-#bd.set_unknown_unicast
-#bd.add_subnet
-#bd.set_multidestination
